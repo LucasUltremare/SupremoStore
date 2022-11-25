96d3d55ffa7ec8692dee6d19d36bd4f1d1724c14
Lucas Ultremare <lucas.ultremare@gmail.com>
2022-11-24 22:48:54 -0300

    Update

--- js/custom.js
+++ js/custom.js
@@ -2,11 +2,7 @@
 
   "use strict";
 
-    // COLOR MODE
-    $('.color-mode').click(function(){
-        $('.color-mode-icon').toggleClass('active')
-        $('body').toggleClass('dark-mode')
-    })
+   
 
     // HEADER
     $(".navbar").headroom();
